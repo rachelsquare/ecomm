b7708eac3436cc378f60760436b79f5bc00c9631
Rachel Skwerer <rachelskwerer@gmail.com>
2021-10-16 16:10:58 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hi there')
+const express = require('express');
+
+const app = express();
+
+// route handler
+// req: object to interact with to receive info from user
+// res: object to interact with to send info to user
+app.get('/', (req, res) => {
+    res.send('hi there!');
+})
+
+// tell application to listen for incoming network traffic
+// on a particular port on the computer
+
+app.listen(3000, () =>  {
+    console.log('Listening');
+})
+
+
