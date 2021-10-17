b7f8743a5d12dbd1af22a7c456f19793ddcf732d
Rachel Skwerer <rachelskwerer@gmail.com>
2021-10-16 23:17:35 -0400

    replaced parser with off the shelf parser

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -18,28 +19,8 @@ app.get('/', (req, res) => {
     `);
 });
 
-// middleware function to parse email, password, passwordConfirmation data
-const bodyParser = (req, res, next) => {
-    if (req.method === 'POST') {
-    req.on('data', data => {
-        const parsed = data.toString('utf8').split('&');
-        const formData = {};
-        for (let pair of parsed) {
-            const [key, value] = pair.split('=');
-            formData[key] = value;
-        }
-        
-        req.body = formData;
-        next();
-    
-     });
-    } else {
-        next();
-    }
-}
 
-
-app.post('/', bodyParser, (req, res) => {
+app.post('/', bodyParser.urlencoded({extended: true}), (req, res) => {
     console.log(req.body);
     // get access to email, password, passwordConfirmation    
     res.send('Account created!!!');
